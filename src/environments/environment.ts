f391637f3e86f78931519e704e6f5b9be300a142
MuhammadFarhanA <farhan45778@gmail.com>
2026-06-27 16:18:57 +0500

    ui changes

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,12 +1,11 @@
 export const environment = {
   production: false,
-  //translateLoaderPrefix: '/i18n/',
-  translateLoaderPrefix: 'https://soletechs.net/property.admin/i18/',
-  apiUrl: 'https://soletechs.net/property.api',
-  wsUrl:  'https://soletechs.net/property.api',  //socket handshake  same server, different path 
+  translateLoaderPrefix: '/i18n/',
+  //  translateLoaderPrefix: 'https://soletechs.net/property.admin/i18/',
+   //apiUrl: 'http://localhost:3000',
+   wsUrl:  'http://localhost:3000',  //socket handshake  same server, different path 
 
-  //apiUrl: 'http://localhost:3000',
-  //wsUrl:  'http://localhost:3000',  //socket handshake  same server, different path 
+  apiUrl: 'https://soletechs.net/property.api',
   newRtcUrl: 'http://localhost:8000',
   geonames: {
     username: 'harissaeed',
